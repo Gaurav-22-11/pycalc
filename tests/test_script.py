7db1e7542e379d3dee2a41c9829d9aceb79ab807
GAURAV DNYANESH MAHAJAN <108111718+Gaurav-22-11@users.noreply.github.com>
2024-03-08 10:04:10 +0530

    Create test_script.py

---
+++ tests/test_script.py
@@ -0,0 +1,22 @@
+import unittest
+from calculator import Calculator
+
+class TestCalc(unittest.Testcase):
+  def setUp(self):
+    self.calculator=Calculator()
+
+  def test_add(self):
+    result=self.calculator.add(4,8)
+    self.assertEqual(result,12)
+
+  def test_subtract(self):
+    result=self.calculator.subtract(8,4)
+    self.assertEqual(result,4)
+
+  def test_multiply(self):
+    result=self.calculator.multiply(4,8)
+    self.assertEqual(result,32)
+
+  def test_divide(self):
+    result=self.calculator.divide(8,4)
+    self.assertEqual(result,2)
