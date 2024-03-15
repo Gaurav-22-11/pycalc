eb322a94d9df337795ffaf819355c44016d1ff73
GAURAV DNYANESH MAHAJAN <108111718+Gaurav-22-11@users.noreply.github.com>
2024-03-15 11:15:16 +0530

    Update test_script1.py

--- tests/test_script1.py
+++ tests/test_script1.py
@@ -1,1 +1,25 @@
+import unittest
+from calculator import Calculator
 
+class TestCalc(unittest.TestCase):
+  def setUp(self):
+    self.calculator=Calculator()
+
+  def test_add(self):
+    result=self.calculator.add(4,8)
+    self.assertEqual(result,12)
+
+  def test_subtract(self):
+    result=self.calculator.subtract(8,4)
+    self.assertEqual(result,4)
+
+  def test_multiply(self):
+    result=self.calculator.multiply(4,8)
+    self.assertEqual(result,36)
+
+  def test_divide(self):
+    result=self.calculator.divide(8,4)
+    self.assertEqual(result,2)
+
+if __name__ == '__main__':
+  unittest.main()
